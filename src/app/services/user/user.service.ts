f1b68bbbfe5d56809fdabc461ad5b0690d5a4edd
Faniry <Samikah23@gmail.com>
2026-06-09 11:56:53 +0300

    fix responsive sigin final v2

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -10,8 +10,8 @@ import { CompletUserHelper } from '../../helpers/complet-user-helper';
 })
 export class UserService {
   private httpClient = inject(HttpClient);
-  // private baseUrl = "https://localhost:7110/";
-  private baseUrl = "https://we-explore-mada.runasp.net/";
+  private baseUrl = "https://localhost:7110/";
+  // private baseUrl = "https://we-explore-mada.runasp.net/";
 
   signup(model: ConfidentialityModel):Observable<UserModel>{
     //console.log('arrive login service')
